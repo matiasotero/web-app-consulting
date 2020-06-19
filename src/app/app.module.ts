899097503013e403d8b18f565becc570b0f47f6a
Matias Otero <motero@beallsinc.com>
2020-06-19 01:50:25 -0300

    project migration to angular in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContainerComponent } from './components/container/container.component';
+import { IntroComponent } from './components/intro/intro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    JumbotronComponent,
+    FooterComponent,
+    ContainerComponent,
+    IntroComponent,
   ],
   imports: [
     BrowserModule,
